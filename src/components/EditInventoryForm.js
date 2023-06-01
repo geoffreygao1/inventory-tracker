5c67a7ff8734b40f67f28ea77ccdf4f36515992c
Geoffrey Gao <geoffreygao1@gmail.com>
2023-06-01 13:48:51 +0900

    Initialized editInventoryForm

---
+++ src/components/EditInventoryForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+
+function EditInventoryForm(props) {
+  const { item } = props;
+
+  function handleEditInventoryFormSubmission(event) {
+    event.preventDefault();
+    props.onEditItem({ names: event.target.names.value, origin: event.target.origin.value, price: event.target.price.value, roast: event.target.roast.value, id: item.id });
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleEditInventoryFormSubmission}
+        buttonText="Update Item" />
+    </React.Fragment>
+  );
+}
+
+EditInventoryForm.propTypes = {
+  item: PropTypes.object,
+  onEditItem: PropTypes.func
+};
+
+export default EditInventoryForm;
